7e8df15a7a6e02624651c7fc48902020b71943f7
rajusw804 <46264561+rajusw804@users.noreply.github.com>
2025-05-05 20:02:02 +0530

    Update init-replicaset.sh

--- init-replicaset.sh
+++ init-replicaset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run mongosh to initiate the replica set
-mongosh --eval 'rs.initiate({ _id: "myReplicaSet", members: [{ _id: 0, host: "192.168.61.10:27017" }, { _id: 1, host: "192.168.61.26:27017" }] })'
+mongosh --eval 'rs.initiate({ _id: "myReplicaSet", members: [{ _id: 0, host: "192.168.61.26:27017" }] })'
